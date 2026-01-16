fc4a916819d8a35202d60a532a46af92932cb991
BhoomiAgrawal12 <bhoomiagrawal1212@gmail.com>
2026-01-16 14:10:48 +0530

    fix: Change  path in UPDATE_SCRIPT.sh and add to mailmap
    Signed-off-by: BhoomiAgrawal12 <bhoomiagrawal1212@gmail.com>

---
+++ update_script.sh
@@ -0,0 +1,15 @@
+# This script shows how to update env.yml from conda-lock.yml
+# Run this whenever conda-lock.yml is updated
+
+set -e
+
+echo "Generating env.yml from conda-lock.yml"
+conda-lock render --kind explicit
+
+mv conda-linux-64.lock env.yml
+
+echo " env.yml updated with full package URLs and hashes"
+
+rm -f conda-osx-64.lock conda-osx-arm64.lock conda-linux-64.lock.yml conda-osx-64.lock.yml conda-osx-arm64.lock.yml
+
+wc -l env.yml
